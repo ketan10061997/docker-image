620de7593b2bf9629a8fa8abfdb3fe305ea04225
ketan10061997 <53621026+ketan10061997@users.noreply.github.com>
2020-04-30 00:17:44 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flaskext.mysql import MySQL
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'ketan'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Ketan@6198'
-app.config['MYSQL_DATABASE_DB'] = 'myUsers'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
